cccdee062de130cc7caeb8919cf80893bb910803
Christopher Horrell <christopher@joyent.com>
2015-03-04 09:41:01 -0500

    Install mdata-* scripts and man pages

--- src/install-tools.sh
+++ src/install-tools.sh
@@ -51,6 +51,8 @@ print_prompt() {
 install_tools() {
   echo "Installing SmartOS VM Guest Tools..."
   cp -r ./lib/smartdc /lib/
+  cp -r ./usr/sbin/mdata-* /usr/sbin/
+  cp -r ./usr/share/man/man1/mdata-* /usr/share/man/man1/
   mv /etc/rc.local /etc/rc.local-backup
   ln -s /lib/smartdc/joyent_rc.local /etc/rc.local
 }
