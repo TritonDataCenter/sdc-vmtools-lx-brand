bb52a4e254f4ca95f2ee2194733972ce44441509
Christopher Horrell <christopher@joyent.com>
2015-03-26 17:26:50 -0400

    Install.sh script takes option -i
    This specify the Install directory to use. The guest tools etc will be
    installed in this directory.
    
    Also, no longer copy man pages (this was missed from a previous commit)

--- src/install.sh
+++ src/install.sh
@@ -3,10 +3,53 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+function usage() {
+cat <<EOF
+
+  Usage: $0 -i {absolute path to installation}
+  e.g. $0 -i /data/chroot
+
+  Install the lx-brand guest tools for a given installation path.
+  The directory path must be absolute with not trailing slashes.
+
+  OPTIONS:
+  -i The path to the given Linux installation
+  -h Show this message
+
+EOF
+}
+
+while getopts "hi:" OPTION
+do
+  case $OPTION in
+    h)
+      usage
+      exit 1
+      ;;
+    i)
+      INSTALL_DIR="$OPTARG"
+      ;;
+    ?)
+      usage
+      exit
+      ;;
+  esac
+done
+
 function fatal() {
   printf "%s\n" "$@"
   exit 1
 }
+
+if [[ ! -a "$INSTALL_DIR" ]]; then
+  pfatal"==> Directory $INSTALL_DIR not found"
+  exit 1
+fi
+
+if [[ "$(echo $INSTALL_DIR | cut -c 1)" != "/" ]]; then
+  fatal "==> The specified install path must be absolute"
+  exit 1
+fi
 
 function install_tools() {
   echo "Creating symlinks for binaries found in /native (e.g., mdata-*, dtrace, prstat etc.)"
@@ -15,43 +58,41 @@ function install_tools() {
   NATIVE_USR_BIN=$(cat ./native_usr_bin.txt)
   
   for binary in $NATIVE_USR_BIN; do
-    ln -s /native/usr/bin/${binary} /usr/bin/${binary}
+    chroot $INSTALL_DIR ln -s /native/usr/bin/${binary} /usr/bin/${binary}
   done
   
   # /native/usr/sbin
   NATIVE_USR_SBIN=$(cat ./native_usr_sbin.txt)
   
   for binary in $NATIVE_USR_SBIN; do
-    ln -s /native/usr/sbin/${binary} /usr/sbin/${binary}
+    chroot $INSTALL_DIR ln -s /native/usr/sbin/${binary} /usr/sbin/${binary}
   done
   
-  echo "Copying mdata-* man pages"
-  cp -r ./usr/share/man/man1/mdata-* /usr/share/man/man1/
-  
   echo "Adding wrapper scripts"
-  cp ./usr/bin/* /usr/bin/
-  cp ./usr/sbin/* /usr/sbin/
+  cp ./usr/bin/* $INSTALL_DIR/usr/bin/
+  cp ./usr/sbin/* $INSTALL_DIR/usr/sbin/
   
   echo "Adding /native/usr/share/man to manpath"
   # This should make most of the man pages in /native available
   # for the symlinks we added
-  echo "MANPATH /native/usr/share/man" >> /etc/man.config
+  echo "/n" >> $INSTALL_DIR/etc/man.config
+  echo "MANPATH /native/usr/share/man" >> $INSTALL_DIR/etc/man.config
 }
 
 function install_debian() {
   install_tools
-  echo "Installing custom rc.local file to /etc/rc.local..."
-  cp ./lib/smartdc/joyent_rc.local /etc/rc.local
+  echo "Installing custom rc.local file to $INSTALL_DIR/etc/rc.local..."
+  cp ./lib/smartdc/joyent_rc.local $INSTALL_DIR/etc/rc.local
 }
 
 function install_redhat() {
   install_tools
-  echo "Installing custom rc.local file to /etc/rc.d/rc.local..."
-  cp ./lib/smartdc/joyent_rc.local /etc/rc.d/rc.local
+  echo "Installing custom rc.local file to $INSTALL_DIR/etc/rc.d/rc.local..."
+  cp ./lib/smartdc/joyent_rc.local $INSTALL_DIR/etc/rc.d/rc.local
   
   # On CentOS 7 systemd is the default.
   # make /etc/rc.d/rc.local executable to enable rc.local Compatibility unit
-  chmod 755 /etc/rc.d/rc.local
+  chmod 755 $INSTALL_DIR/etc/rc.d/rc.local
 }
 
 if [[ $EUID -ne 0 ]] ; then
@@ -64,16 +105,16 @@ OS=$(uname -s)
 
 case $OS in
   Linux)
-    if [[ -f /etc/redhat-release ]] ; then
+    if [[ -f $INSTALL_DIR/etc/redhat-release ]] ; then
       install_redhat
-    elif [[ -f /etc/debian_version ]] ; then
+    elif [[ -f $INSTALL_DIR/etc/debian_version ]] ; then
       install_debian
     else
-      fatal "Sorry. Your OS ($OS) is not supported by this installer"
+      fatal "Sorry. Your OS ($OS) is not supported."
     fi
     ;;
   *)
-    fatal "Sorry. Your OS ($OS) is not supported by this installer"
+    fatal "Sorry. Your OS ($OS) is not supported."
     ;;
 esac
 
