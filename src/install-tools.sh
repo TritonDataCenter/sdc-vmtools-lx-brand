68fa78b68ded6fa691986ddb7c9542123573b7ff
Christopher Horrell <christopher@joyent.com>
2015-03-26 09:44:58 -0400

    First cut of /native symlinks

--- src/install-tools.sh
+++ src/install-tools.sh
@@ -49,11 +49,33 @@ print_prompt() {
 }
 
 install_tools() {
-  echo "Creating mdata-* symlinks..."
+  echo "Creating symlinks for binaries found in /native (e.g., mdata-*, dtrace, prstat etc.)"
+  # /native/usr/bin
+  ln -s /native/usr/bin/mdb /usr/bin/mdb
+  ln -s /native/usr/bin/pcred /usr/bin/pcred
+  ln -s /native/usr/bin/pfiles /usr/bin/pfiles
+  ln -s /native/usr/bin/pflags /usr/bin/pflags
+  ln -s /native/usr/bin/pldd /usr/bin/pldd
+  ln -s /native/usr/bin/prstat /usr/bin/prstat
+  ln -s /native/usr/bin/prun /usr/bin/prun
+  ln -s /native/usr/bin/psig /usr/bin/psig
+  ln -s /native/usr/bin/pstack /usr/bin/pstack
+  ln -s /native/usr/bin/pstop /usr/bin/pstop
+  ln -s /native/usr/bin/ptime /usr/bin/ptime
+  ln -s /native/usr/bin/pwait /usr/bin/pwait
+  ln -s /native/usr/bin/pwdx /usr/bin/pwdx
+  ln -s /native/usr/bin/truss /usr/bin/truss
+  ln -s /native/usr/bin/kstat /usr/bin/kstat
+  
+  # /native/sbin
+  ln -s /native/usr/sbin/cpustat /usr/sbin/cpustat
+  ln -s /native/usr/sbin/dtrace /usr/sbin/dtrace
   ln -s /native/usr/sbin/mdata-get /usr/sbin/mdata-get
   ln -s /native/usr/sbin/mdata-put /usr/sbin/mdata-put
   ln -s /native/usr/sbin/mdata-delete /usr/sbin/mdata-delete
   ln -s /native/usr/sbin/mdata-list /usr/sbin/mdata-list
+  ln -s /native/usr/sbin/plockstat /usr/sbin/plockstat
+  
   
   echo "Copying mdata-* man pages"
   cp -r ./usr/share/man/man1/mdata-* /usr/share/man/man1/
