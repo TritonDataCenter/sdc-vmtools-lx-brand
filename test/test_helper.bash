cdad2115d762150ab82655c1047c7b99f23de96b
Christopher Horrell <christopher@joyent.com>
2015-04-17 09:27:35 -0400

    Mock up test environment

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,7 +1,47 @@
 setup() {
-  mkdir tmp
+  
+  # Create mock up test environment in TMP
+  
+  TMP=tmp
+  
+  
+  # Create all the expected directories
+  mkdir -p $TMP/etc/rc.d/
+  mkdir -p $TMP/lib
+  mkdir -p $TMP/native/usr/bin
+  mkdir -p $TMP/native/usr/sbin
+  
+  # Create /native symlinks for content in src/symlinks.txt
+  SYMLINKS=$(cat ./src/symlinks.txt)
+  for binary in $SYMLINKS; do
+    ln -s /native${binary} $TMP/${binary}
+  done
+  
+  # Create /native symlinks for content in src/wrappers.txt
+  WRAPPERS=$(cat ./src/wrappers.txt)
+  for wrapper in $WRAPPERS; do
+    binary=$(echo ${wrapper} | cut -f1 -d' ')
+    ln -s /native${binary} $TMP/${binary}
+  done
+  
+  OS=$(uname -s)
+
+  case $OS in
+    Linux)
+      if [[ -f /etc/redhat-release ]] ; then
+        cp /etc/redhat-release $TMP/etc/redhat-release
+      elif [[ -f /etc/debian_version ]] ; then
+        cp /etc/debian_version $TMP/etc/debian_version
+      else
+        fatal "$OS is not supported."
+      fi
+      ;;
+    *)
+      fatal "$OS is not supported."
+      ;;
+  esac
 }
 
 teardown() {
-  [ -d "tmp" ] && rm -rf "tmp"
+  [ -d "$TMP" ] && rm -rf "$TMP"
 }
